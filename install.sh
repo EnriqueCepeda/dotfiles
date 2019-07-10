fab82f886e05074d2f45a9b1e19caf6b6239014e
Enrique Cepeda <ecepedavillamayor@gmail.com>
2019-07-10 17:46:12 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install zsh curl git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mv zshrc ~/.zshrc 
+sudo apt-get install python3-pip
+pip3 install virtualenv virtualenvwrapper
+
