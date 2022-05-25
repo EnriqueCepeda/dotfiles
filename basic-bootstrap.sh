085f22cffe03d9388805b33d0d45195103a3747f
Enrique Cepeda <ecepedavillamayor@gmail.com>
2022-05-25 14:51:57 +0200

    fix: virtualenvwrapper installation permissions

--- basic-bootstrap.sh
+++ basic-bootstrap.sh
@@ -9,5 +9,5 @@ cp ~/dotfiles/.gitmessage.txt ~/
 chmod +x ./basic-bootstrap.sh ./oh-my-zsh.sh ./restart-config.sh ./.zshrc ~/.env.zsh ~/.keys.zsh
 
 sudo apt install zsh tmux tmuxinator python3-pip python3
-pip3 install virtualenvwrapper
+sudo -H pip3 install virtualenvwrapper
 chsh -s $(which zsh)
